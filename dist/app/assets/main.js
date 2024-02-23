877eebea414ba22ca1ff390ab18b8f2446835109
Caleb Simpson <caleb@helloplaybook.com>
2024-02-23 16:47:19 -0500

    Dont flash 'not found' so early when loading a note

--- dist/app/assets/main.js
+++ dist/app/assets/main.js
@@ -18816,13 +18816,21 @@ window.addEventListener(PageContext.NOTE_IN_FOCUS_CHANGED, async function(e) {
     const stubTitle = PageContext.instance.noteTitleFromUrl();
     const note = PageContext.instance.note;
     if (note.nostrEvent) {
-        $("#note-content").html(MarkdownRenderer.instance.renderHtml(note.content));
+        const renderedContent = MarkdownRenderer.instance.renderHtml(note.content);
+        const html = note.private ? `<div style="font-weight:bold; text-align: center; color: #aa0000">⚠️ This note is private and cannot be viewed by others.</div>${renderedContent}` : renderedContent;
+        $("#note-content").html(html);
         window.MDEditor.value(note.content);
         loadBackrefs();
-    } else if (!!stubTitle) {
-        $("#note-content").html(`<h1>${stubTitle}</h1><p>⚠️ This note is a stub and does not exist yet. Click "open in editor" to start writing!</p>`);
     } else {
-        $("#note-content").html("<center><h3>note not found!</h3>Either this version of the note no longer exists or it's on a different nostr relay.</center>");
+        const content = stubTitle
+            ? `<h1>${stubTitle}</h1><p>⚠️ This note is a stub and does not exist yet. Click "open in editor" to start writing!</p>`
+            : "<center><h3>note not found!</h3>Either this version of the note no longer exists or it's on a different nostr relay.</center>"
+            ;
+
+        setTimeout(() => {
+            if (PageContext.instance.note.nostrEvent) { return; } // If the note has been loaded by now, don't overwrite it.
+            $("#note-content").html(content);
+        }, 2000);
     }
 
     if (!!window.notesModal) { window.notesModal.hide(); }
