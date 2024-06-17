e88aac34c795646cb974d0fc8b37ab927c3fa517
Caleb Simpson <caleb@helloplaybook.com>
2024-06-17 11:40:44 -0400

    Use nip04 fallback if nip44 is not available

--- src/common.js
+++ src/common.js
@@ -204,8 +204,10 @@ export function atagFor(title, hexpubkey) {
 }
 
 export async function encryptSelf(text) {
-  if (!!window.nostr && !!window.nostr.nip44) {
+  if (!!window.nostr?.nip44) {
     return window.nostr.nip44.encrypt(window.nostrUser.hexpubkey, text);
+  } else if (!!window.nostr?.nip04) {
+    return window.nostr.nip04.encrypt(window.nostrUser.hexpubkey, text);
   } else if (!!window.sessionStorage.privateKey) {
     return Promise.resolve(crypto.AES.encrypt(text, window.sessionStorage.privateKey).toString());
   } else {
@@ -214,8 +216,10 @@ export async function encryptSelf(text) {
 }
 
 export async function decryptSelf(text) {
-  if (!!window.nostr && !!window.nostr.nip44) {
+  if (!!window.nostr?.nip44) {
     return window.nostr.nip44.decrypt(window.nostrUser.hexpubkey, text);
+  } else if (!!window.nostr?.nip04) {
+    return window.nostr.nip04.decrypt(window.nostrUser.hexpubkey, text);
   } else if (!!window.sessionStorage.privateKey) {
     return Promise.resolve(crypto.AES.decrypt(text, window.sessionStorage.privateKey).toString(crypto.enc.Utf8));
   } else {
